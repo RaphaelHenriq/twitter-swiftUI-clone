f54e69da9a28214439b85fe08f13d57cf3b9bf66
Raphael Henrique <45474283+RaphaelHenriq@users.noreply.github.com>
2023-08-09 10:16:44 -0300

    Create a new project

---
+++ TwitterSwiftUIClone/TwitterSwiftUICloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUICloneApp.swift
+//  TwitterSwiftUIClone
+//
+//  Created by Raphael Henrique on 09/08/23.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUICloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
