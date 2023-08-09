79b42eb2e9c1bd7b079b3d849daf3535a8137244
brw.rhenrique <brw.rhenrique@ifood3rd.com>
2023-08-09 16:13:23 -0300

    create tabbar

---
+++ TwitterSwiftUIClone/Core/Feed/ViewModels/Tweets/TweetRowView.swift
@@ -0,0 +1,72 @@
+import SwiftUI
+
+struct TweetRowView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            HStack(alignment: .top, spacing: 12) {
+                Circle()
+                    .frame(width: 56, height: 56)
+                VStack(alignment: .leading, spacing: 4) {
+                    HStack {
+                        Text("Bruce Wayne")
+                            .font(.subheadline).bold()
+                        Text("@batman")
+                            .foregroundColor(.gray)
+                            .font(.caption)
+                        Text("2w")
+                            .foregroundColor(.gray)
+                            .font(.caption)
+                    }
+                    Text("I believe in Harvey Dent")
+                        .font(.subheadline)
+                        .multilineTextAlignment(.leading)
+                }
+            }
+            HStack(alignment: .firstTextBaseline) {
+                Button {
+                    print("")
+                } label: {
+                    Image(systemName: "bubble.left")
+                        .font(.subheadline)
+                }
+                
+                Spacer()
+                
+                Button {
+                    print("")
+                } label: {
+                    Image(systemName: "arrow.2.squarepath")
+                        .font(.subheadline)
+                }
+                
+                Spacer()
+                
+                Button {
+                    print("")
+                } label: {
+                    Image(systemName: "heart")
+                        .font(.subheadline)
+                }
+                
+                Spacer()
+                
+                Button {
+                    print("")
+                } label: {
+                    Image(systemName: "bookmark")
+                        .font(.subheadline)
+                }
+
+            }.padding()
+            Divider()
+        }
+        .padding()
+        
+    }
+}
+
+struct TweetRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        TweetRowView()
+    }
+}
--- TwitterSwiftUIClone/TwitterSwiftUICloneApp.swift
+++ TwitterSwiftUIClone/TwitterSwiftUICloneApp.swift
@@ -1,17 +1,10 @@
-//
-//  TwitterSwiftUICloneApp.swift
-//  TwitterSwiftUIClone
-//
-//  Created by Raphael Henrique on 09/08/23.
-//
-
 import SwiftUI
 
 @main
 struct TwitterSwiftUICloneApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            FeedView()
         }
     }
 }
